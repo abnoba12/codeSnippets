f2cac2732d79010a9751cdcbea18ec26e1ae09d4
Jacob Weigand <Jacob.Weigand@smartstartinc.com>
2017-09-21 08:18:13 -0500

    Adding ContainsElementsAttribute

---
+++ Programming Languages/C Sharp/MVC/Custom Validation Attributes/ContainsElementsAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SmartWeb.Web.Helpers
+{
+    /// <summary>
+    /// Validator to ensure the list has at least X number of elements in it
+    /// EX: [ContainsMinElements(1, ErrorMessage = "At least one test window is required")]
+    /// </summary>
+    public class ContainsMinElementsAttribute : ValidationAttribute
+    {
+        private readonly int _minElements;
+        public ContainsMinElementsAttribute(int minElements)
+        {
+            _minElements = minElements;
+        }
+
+        public override bool IsValid(object value)
+        {
+            var list = value as IList;
+            if (list != null)
+            {
+                return list.Count >= _minElements;
+            }
+            return false;
+        }
+    }
+}
