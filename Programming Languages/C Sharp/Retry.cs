1aa5975e668dfbd5030fb84f6591511c5b8a84ba
Jacob Weigand <jacob.weigand@smartstartinc.com>
2022-07-28 07:59:56 -0500

    retry logic

---
+++ Programming Languages/C Sharp/Retry.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ClientDocument
+{
+
+    /// <summary>
+    /// Usage: 
+    /// 1. Retry.Do(() => SomeFunctionThatCanFail(), TimeSpan.FromSeconds(1));
+    /// 2. Retry.Do(SomeFunctionThatCanFail, TimeSpan.FromSeconds(1));
+    /// 3. int result = Retry.Do(SomeFunctionWhichReturnsInt, TimeSpan.FromSeconds(1), 4);
+    /// </summary>
+    public static class Retry
+    {
+        public static void Do(
+            Action action,
+            TimeSpan retryInterval,
+            int maxAttemptCount = 3)
+        {
+            Do<object>(() =>
+            {
+                action();
+                return null;
+            }, retryInterval, maxAttemptCount);
+        }
+
+        /// <summary>
+        /// USAGE: int result = Retry.Do(SomeFunctionWhichReturnsInt, TimeSpan.FromSeconds(1), 4);
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="action"></param>
+        /// <param name="retryInterval"></param>
+        /// <param name="maxAttemptCount"></param>
+        /// <returns></returns>
+        /// <exception cref="AggregateException"></exception>
+        public static T Do<T>(
+            Func<T> action,
+            TimeSpan retryInterval,
+            int maxAttemptCount = 3)
+        {
+            var exceptions = new List<Exception>();
+
+            for (int attempted = 0; attempted < maxAttemptCount; attempted++)
+            {
+                try
+                {
+                    if (attempted > 0)
+                    {
+                        Thread.Sleep(retryInterval);
+                    }
+                    return action();
+                }
+                catch (Exception ex)
+                {
+                    exceptions.Add(ex);
+                }
+            }
+            throw new AggregateException(exceptions);
+        }
+
+        public static async Task DoAsync(
+            Action action,
+            TimeSpan retryInterval,
+            int maxAttemptCount = 3)
+        {
+            await DoAsync<object>(() =>
+            {
+                action();
+                return null;
+            }, retryInterval, maxAttemptCount);
+        }
+
+        public static async Task<T> DoAsync<T>(
+            Func<Task<T>> action,
+            TimeSpan retryInterval,
+            int maxAttemptCount = 3)
+        {
+            var exceptions = new List<Exception>();
+
+            for (int attempted = 0; attempted < maxAttemptCount; attempted++)
+            {
+                try
+                {
+                    if (attempted > 0)
+                    {
+                        Thread.Sleep(retryInterval);
+                    }
+                    return await action();
+                }
+                catch (Exception ex)
+                {
+                    exceptions.Add(ex);
+                }
+            }
+            throw new AggregateException(exceptions);
+        }
+    }
+}
