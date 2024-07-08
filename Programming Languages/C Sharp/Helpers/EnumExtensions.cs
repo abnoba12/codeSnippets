36fdc29b132e8e0421ab9921a12b96b729d117cc
Jacob Weigand <jacob.weigand@smartstartinc.com>
2024-07-08 13:55:40 -0500

    Nice little enum helper

---
+++ Programming Languages/C Sharp/Helpers/EnumExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Reflection;
+
+namespace SmartStartWebAPI.Business.Helpers
+{
+    [AttributeUsage(AttributeTargets.Field, Inherited = false, AllowMultiple = false)]
+    sealed class EnumDisplayNameAttribute : Attribute
+    {
+        public string DisplayName { get; }
+
+        public EnumDisplayNameAttribute(string displayName) => DisplayName = displayName;
+    }
+
+    public static class EnumExtensions
+    {
+        /// <summary>
+        /// Decorate your enum with your display name [EnumDisplayName("Smart Start")]. Then on your enum call company.GetDisplayName() and you will get back your EnumDisplayName.
+        /// This allows spaces in your enum names.
+        /// </summary>
+        /// <param name="enumValue"></param>
+        /// <returns></returns>
+        public static string GetDisplayName(this Enum enumValue)
+        {
+            var enumType = enumValue.GetType();
+            var memberInfos = enumType.GetMember(enumValue.ToString());
+            if (memberInfos.Length > 0)
+            {
+                var customAttributes = memberInfos[0].GetCustomAttributes(typeof(EnumDisplayNameAttribute), false);
+                if (customAttributes.Length > 0)
+                {
+                    return ((EnumDisplayNameAttribute)customAttributes[0]).DisplayName;
+                }
+            }
+            return enumValue.ToString();
+        }
+    }
+}
