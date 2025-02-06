6a6ef81a559c6ad87e3f03b95b97ffc308b5347a
Jacob Weigand <jacob.weigand@smartstartinc.com>
2025-02-06 10:46:14 -0600

    moving

--- Programming Languages/C Sharp/CachedDataManager.cs
+++ Programming Languages/C Sharp/CachedDataManager.cs
@@ -6,136 +6,290 @@ using System.Data.SqlClient;
 using System.Configuration;
 using System.Collections.Concurrent;
 using System.Web.Hosting;
-using System.Collections.Generic;
 using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 
 /// <summary>
-///  Wrapper for the HttpRuntime Cache.
+/// Wrapper for the HttpRuntime Cache.
 /// </summary>
 public static class CachedDataManager
 {
+    // Locks used to synchronize cache insertions by key.
     private static readonly ConcurrentDictionary<string, object> cacheItemLocks = new ConcurrentDictionary<string, object>(StringComparer.Ordinal);
-    
+
     /// <summary>
-    /// Cache time in seconds
-    /// Default and shortest ttl is 1 minute if one is not set in the webconfg
-    /// <add key = "cacheTimeout"  value="43200"/><!--Server cache time 12 hours -->
+    /// Cache timeout in seconds.
+    /// The default (and minimum) TTL is 60 seconds if one is not set in the configuration.
+    /// Example in web.config:
+    ///   <add key="cacheTimeout" value="43200"/><!-- Server cache time 12 hours -->
     /// </summary>
     private static readonly Lazy<int> cacheTimeout = new Lazy<int>(() =>
     {
-        int timeout = int.TryParse(ConfigurationManager.AppSettings["cacheTimeout"], out timeout) ? timeout : 0;
-        return Math.Max(timeout, 60);
+        return int.TryParse(ConfigurationManager.AppSettings["cacheTimeout"], out int timeout)
+            ? Math.Max(timeout, 60)
+            : 60;
     });
 
+    /// <summary>
+    /// Gets a value stored in the current HttpContext.Items collection.
+    /// If the value does not exist and a function is provided, it will be executed and stored in the Items collection.
+    /// </summary>
+    /// <typeparam name="T">The type of the item.</typeparam>
+    /// <param name="contextKey">The key used to reference the item in HttpContext.Items.</param>
+    /// <param name="getValue">Optional. A function to retrieve the value if it is not already present.</param>
+    /// <returns>The item of type T, or the default value if none exists and no getValue function is provided.</returns>
     public static T GetContextItem<T>(string contextKey, Func<T> getValue = null)
     {
-        var tup = HttpContext.Current.Items[contextKey] as Tuple<T>;
-        if (tup != null)
+        if (HttpContext.Current.Items[contextKey] is Tuple<T> tup)
+        {
             return tup.Item1;
+        }
 
         if (getValue == null)
-            return default(T);
+        {
+            return default;
+        }
 
         tup = Tuple.Create(getValue());
         HttpContext.Current.Items[contextKey] = tup;
-
         return tup.Item1;
     }
 
     /// <summary>
-    /// First the cache is checked to see if there is a value with the matching key. If there is no value then the function inside of
-    /// the getValue parameter is called and it's return value is placed inside of the cache with the provided key.
-    /// Usage: 
-    ///     string test = CachedDataManager.GetCachedItem<String>("testCache", () => {
-    ///         return String.Format("this is a {0} string", "test");
-    ///     }, new TimeSpan(1, 0, 0));
+    /// Retrieves an item from the HttpRuntime.Cache with the specified key. If the item does not exist, it calls the provided function
+    /// to obtain the value, caches it, and then returns it.
     /// </summary>
-    /// <typeparam name="T"></typeparam>
-    /// <param name="cacheKey">The key used to reference the cached value</param>
-    /// <param name="getValue">Value to be saved in the cache</param>
-    /// <param name="timeoutLength">(Optional) Set the time to live for this specific cache key</param>
-    /// <param name="dontCacheEmpty">(Optional) If the result of "getValue" is empty or the default value then do not save those results in the cache. Default = false</param>
-    /// <param name="dependsOnKeys">(Optional) If this cache key is dependent on other keys then they can be linked usinging this parameter. When any of these resources changes, the cached object becomes obsolete and is removed from the cache.
-    /// list. This will force the expiration </param>
-    /// <returns></returns>
+    /// <typeparam name="T">The type of the item to cache.</typeparam>
+    /// <param name="cacheKey">The key used to reference the cached value.</param>
+    /// <param name="getValue">A function to retrieve the value if it is not in the cache.</param>
+    /// <param name="timeoutLength">
+    /// Optional. The time-to-live for the cached item. If not provided, the configured cacheTimeout (with a slight random jitter) is used.
+    /// </param>
+    /// <param name="dontCacheEmpty">
+    /// Optional. If set to true, and the result of getValue is empty or the default value, the value is not cached. Default is false.
+    /// </param>
+    /// <param name="dependsOnKeys">
+    /// Optional. An array of keys that the cached item depends on. If any of these change, the cached item becomes invalid.
+    /// </param>
+    /// <returns>The cached item of type T, or the default value if not found and no getValue function is provided.</returns>
     public static T GetCachedItem<T>(string cacheKey, Func<T> getValue = null, TimeSpan? timeoutLength = null, bool dontCacheEmpty = false, params string[] dependsOnKeys)
     {
-        var tup = HttpRuntime.Cache[cacheKey] as Tuple<T>;
-        if (tup != null)
+        // Try to get from cache.
+        if (HttpRuntime.Cache[cacheKey] is Tuple<T> tup)
+        {
             return tup.Item1;
+        }
 
         if (getValue == null)
-            return default(T);
+        {
+            return default;
+        }
 
+        // Variable to hold the value to be returned.
+        T result = default;
+
+        // Synchronize cache population for this key.
         lock (cacheItemLocks.GetOrAdd(cacheKey, _ => new object()))
         {
-            tup = HttpRuntime.Cache[cacheKey] as Tuple<T>;
-            if (tup == null)
+            // Double-check the cache in case it was added while waiting for the lock.
+            if (HttpRuntime.Cache[cacheKey] is Tuple<T> cachedTup)
             {
-                T val = getValue();
+                return cachedTup.Item1;
+            }
 
-                //Don't cache this item because we got no result from the function getValue.
-                if (dontCacheEmpty && !HasValues(val))
-                {
-                    return tup.Item1;
-                }
+            // Retrieve fresh value.
+            result = getValue();
 
-                if (val != null)
-                {
-                    tup = Tuple.Create(val);
-                    var dependency = dependsOnKeys.Length > 0 ? new CacheDependency(null, dependsOnKeys) : null;
-                    var timeout = timeoutLength == null ? DateTime.Now.AddSeconds(cacheTimeout.Value + (new Random()).Next(-30, 31)) : DateTime.Now.AddMilliseconds(timeoutLength.Value.TotalMilliseconds);
-                    timeout = dependency == null ? timeout : Cache.NoAbsoluteExpiration;
-                    HttpRuntime.Cache.Insert(cacheKey, tup, dependency, timeout, Cache.NoSlidingExpiration, CacheItemPriority.NotRemovable, null);
-                }
+            // If dontCacheEmpty is true and the value is considered empty, return it without caching.
+            if (dontCacheEmpty && !HasValues(result))
+            {
+                return result;
+            }
+
+            // If a valid value is returned, cache it.
+            if (result != null)
+            {
+                tup = Tuple.Create(result);
+                var dependency = dependsOnKeys.Length > 0 ? new CacheDependency(null, dependsOnKeys) : null;
+                DateTime expiration = timeoutLength.HasValue
+                    ? DateTime.Now.Add(timeoutLength.Value)
+                    : DateTime.Now.AddSeconds(cacheTimeout.Value + new Random().Next(-30, 31));
+
+                // If dependencies are provided, let the dependency drive expiration.
+                expiration = dependency == null ? expiration : Cache.NoAbsoluteExpiration;
+
+                HttpRuntime.Cache.Insert(
+                    cacheKey,
+                    tup,
+                    dependency,
+                    expiration,
+                    Cache.NoSlidingExpiration,
+                    CacheItemPriority.NotRemovable,
+                    null);
             }
         }
-        object o = cacheItemLocks.TryRemove(cacheKey, out o) ? o : null;
 
-        return tup.Item1;
+        // Clean up the lock.
+        cacheItemLocks.TryRemove(cacheKey, out _);
+
+        return result;
     }
 
+    /// <summary>
+    /// Retrieves an XML string from the database by executing a stored procedure, caching the result.
+    /// </summary>
+    /// <param name="connectionStringName">The name of the connection string in the configuration.</param>
+    /// <param name="sprocName">The name of the stored procedure to execute.</param>
+    /// <returns>An XML string returned from the stored procedure, or null if no XML is returned.</returns>
     public static string GetDatabaseXmlString(string connectionStringName, string sprocName)
     {
-        return GetCachedItem(string.Join("_", connectionStringName, sprocName), () =>
+        string cacheKey = $"{connectionStringName}_{sprocName}";
+        return GetCachedItem(cacheKey, () =>
         {
             using (var cn = new SqlConnection(ConfigurationManager.ConnectionStrings[connectionStringName].ConnectionString))
             {
                 cn.Open();
-                var cmd = new SqlCommand(sprocName, cn) { CommandType = CommandType.StoredProcedure };
-                var rdr = cmd.ExecuteXmlReader();
-                return rdr.Read() ? rdr.ReadOuterXml() : null;
+                using (var cmd = new SqlCommand(sprocName, cn) { CommandType = CommandType.StoredProcedure })
+                {
+                    using (var rdr = cmd.ExecuteXmlReader())
+                    {
+                        return rdr.Read() ? rdr.ReadOuterXml() : null;
+                    }
+                }
             }
         });
     }
 
+    /// <summary>
+    /// Clears all entries from the HttpRuntime.Cache.
+    /// </summary>
     public static void ClearCache()
     {
-        foreach (System.Collections.DictionaryEntry entry in HttpRuntime.Cache)
+        foreach (DictionaryEntry entry in HttpRuntime.Cache)
         {
             HttpRuntime.Cache.Remove((string)entry.Key);
         }
     }
 
+    /// <summary>
+    /// Removes the cached item with the specified key from the HttpRuntime.Cache.
+    /// </summary>
+    /// <param name="cacheKey">The key of the cached item to remove.</param>
     public static void RemoveCachedItem(string cacheKey)
     {
         if (HttpRuntime.Cache[cacheKey] != null)
+        {
             HttpRuntime.Cache.Remove(cacheKey);
+        }
     }
 
+    /// <summary>
+    /// Gets a versioned URL for a virtual path by appending a query string parameter based on the file hash.
+    /// </summary>
+    /// <param name="path">The virtual path to version.</param>
+    /// <returns>The versioned URL as a string.</returns>
     public static string HrefVersioned(string path)
     {
-        var VirtualPath = HttpRuntime.AppDomainAppVirtualPath;
+        string virtualPath = HttpRuntime.AppDomainAppVirtualPath;
         if (!VirtualPathUtility.IsAppRelative(path))
-            path = VirtualPathUtility.Combine(VirtualPath, path);
+        {
+            path = VirtualPathUtility.Combine(virtualPath, path);
+        }
 
         if (HttpRuntime.Cache[path] == null)
-            HttpRuntime.Cache.Insert(path, HostingEnvironment.VirtualPathProvider.GetFileHash(path, new string[] { path }), HostingEnvironment.VirtualPathProvider.GetCacheDependency(path, new string[] { path }, DateTime.Now.ToUniversalTime()));
+        {
+            HttpRuntime.Cache.Insert(
+                path,
+                HostingEnvironment.VirtualPathProvider.GetFileHash(path, new[] { path }),
+                HostingEnvironment.VirtualPathProvider.GetCacheDependency(path, new[] { path }, DateTime.Now.ToUniversalTime()));
+        }
 
-        return string.Concat(VirtualPathUtility.ToAbsolute(path), "?v", HttpRuntime.Cache[path]);
+        return $"{VirtualPathUtility.ToAbsolute(path)}?v{HttpRuntime.Cache[path]}";
     }
 
+    /// <summary>
+    /// Executes the provided function on every call to get fresh data.
+    /// Under normal operation, the function is executed, its result is cached (or the cache is updated),
+    /// and then the fresh data is returned.
+    /// If the function throws an exception, the method attempts to return a previously cached value as a fallback.
+    /// The cache duration is specified in days.
+    /// </summary>
+    /// <typeparam name="T">The type of the item to cache.</typeparam>
+    /// <param name="cacheKey">The key used to reference the cached value.</param>
+    /// <param name="getValue">A function to retrieve the value for fresh data.</param>
+    /// <param name="cacheDays">The number of days to cache the value.</param>
+    /// <param name="dontCacheEmpty">
+    /// Optional. If set to true, and the result of getValue is empty or the default value, the value is not cached.
+    /// Default is false.
+    /// </param>
+    /// <param name="dependsOnKeys">
+    /// Optional. An array of keys that the cached item depends on. Changes to any of these keys will invalidate the cached item.
+    /// </param>
+    /// <returns>The fresh data of type T if retrieval is successful, or the cached data as a fallback if retrieval fails.</returns>
+    public static T GetCachedItemAsFallback<T>(string cacheKey, Func<T> getValue, int cacheDays, bool dontCacheEmpty = false, params string[] dependsOnKeys)
+    {
+        // Try to get fresh data.
+        T result;
+        try
+        {
+            result = getValue();
+
+            // If dontCacheEmpty is true and the result is empty, simply return the result without caching.
+            if (dontCacheEmpty && !HasValues(result))
+            {
+                return result;
+            }
+
+            // If a valid result was returned, cache (or update) it.
+            if (result != null)
+            {
+                // Lock to synchronize cache updates.
+                lock (cacheItemLocks.GetOrAdd(cacheKey, _ => new object()))
+                {
+                    var tup = Tuple.Create(result);
+                    var dependency = dependsOnKeys.Length > 0 ? new CacheDependency(null, dependsOnKeys) : null;
+                    // Use the specified cacheDays to calculate the expiration.
+                    DateTime expiration = DateTime.Now.AddDays(cacheDays);
+                    // If dependencies are provided, let the dependency drive expiration.
+                    if (dependency != null)
+                    {
+                        expiration = Cache.NoAbsoluteExpiration;
+                    }
+                    HttpRuntime.Cache.Insert(
+                        cacheKey,
+                        tup,
+                        dependency,
+                        expiration,
+                        Cache.NoSlidingExpiration,
+                        CacheItemPriority.NotRemovable,
+                        null);
+                }
+                // Remove the lock object for this key.
+                cacheItemLocks.TryRemove(cacheKey, out _);
+            }
+
+            return result;
+        }
+        catch (Exception)
+        {
+            // If fresh retrieval fails, try to return a cached fallback value.
+            if (HttpRuntime.Cache[cacheKey] is Tuple<T> cached)
+            {
+                return cached.Item1;
+            }
+            // Otherwise, rethrow the exception.
+            throw;
+        }
+    }
+
+    /// <summary>
+    /// Determines whether the specified value is considered "non-empty."
+    /// It supports checking collections, strings, nullable types, and arrays.
+    /// </summary>
+    /// <typeparam name="T">The type of the value to check.</typeparam>
+    /// <param name="val">The value to check.</param>
+    /// <returns>True if the value is non-empty; otherwise, false.</returns>
     private static bool HasValues<T>(T val)
     {
         if (val == null)
@@ -145,23 +299,19 @@ public static class CachedDataManager
 
         Type type = typeof(T);
 
-        // Check if the type is a collection
+        // Check if the type is a collection (but not a string)
         if (typeof(IEnumerable).IsAssignableFrom(type) && type != typeof(string))
         {
-            IEnumerable enumerable = (IEnumerable)val;
-            if (!enumerable.Cast<object>().Any())
+            if (((IEnumerable)val).Cast<object>().Any() == false)
             {
-                //Value is an empty collection
                 return false;
             }
         }
         // Check if the type is a string
         else if (type == typeof(string))
         {
-            string strVal = val as string;
-            if (string.IsNullOrWhiteSpace(strVal))
+            if (string.IsNullOrWhiteSpace(val as string))
             {
-                //Value is null or whitespace
                 return false;
             }
         }
@@ -170,26 +320,23 @@ public static class CachedDataManager
         {
             if (val.Equals(default(T)))
             {
-                //Value is null or the default value
                 return false;
             }
         }
         // Check if the type is an array
         else if (type.IsArray)
         {
-            Array array = val as Array;
-            if (array != null && array.Length == 0)
+            if (val is Array array && array.Length == 0)
             {
-                //Value is an empty array
                 return false;
             }
         }
         // Check if the value is the default value for the type
         else if (val.Equals(default(T)))
         {
-            //Value is the default value for the type
             return false;
         }
+
         return true;
     }
 }
